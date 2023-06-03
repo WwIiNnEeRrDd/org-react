45e6e2b77567047df3d662567045dfd1e5bca6c6
Richard <richardhpaz@gmail.com>
2023-06-03 02:12:28 -0500

    todo

---
+++ src/componentes/Equipo/Equipo.js
@@ -0,0 +1,34 @@
+import "./Equipo.css"
+import Colaborador from "../Colaborador/Colaborador"
+import hexToRgba from "hex-to-rgba"
+
+const Equipo = (props) => {
+    //destructuracion
+    const { colorPrimario, colorSecundario, titulo, id } = props.datos
+    const {colaboradores, eliminarColaborador, actualizarColor, like} = props
+
+    return <>
+        { 
+            colaboradores.length > 0 &&
+            <section className="equipo" style={{ backgroundColor: hexToRgba(colorPrimario, 0.6)  }}>
+                <input className="input-color" type='color' value={colorPrimario} onChange={(evento)=>{
+                    actualizarColor(evento.target.value, id)
+                }}></input>
+                <h3 style={{ borderColor: colorPrimario }}>{titulo}</h3>
+                <div className="colaboradores">
+                    {
+                        colaboradores.map( (colaborador,index) => <Colaborador 
+                            datos={colaborador} 
+                            key={index} 
+                            colorPrimario={hexToRgba(colorPrimario, 0.6)}
+                            eliminarColaborador={eliminarColaborador}
+                            like={like}
+                        />)
+                    }
+                </div>
+            </section>
+        }
+    </>
+}
+
+export default Equipo
